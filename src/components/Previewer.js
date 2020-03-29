07773b0e890355591ff0b7a6d09158c58c5ac620
sripwoud <me@sripwoud.xyz>
2020-03-29 13:08:32 +0200

    feat: Parse HTML raw text

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import marked from 'marked'
 const Previewer = props => {
+  const getMarkdownText = () => {
+    const rawMarkup = marked(props.content, { sanitize: true })
+    return { __html: rawMarkup }
+  }
+
   return (
     <div className='ui segment'>
       <h2 className='ui header'>Previewer</h2>
       <div className='ui container'>
-        {props.content}
+        <div
+          dangerouslySetInnerHTML={getMarkdownText()}
+        />
       </div>
     </div>
   )
