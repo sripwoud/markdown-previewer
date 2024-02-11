a908703fad6cd47550871321460d107441390e54
sripwoud <me@sripwoud.xyz>
2024-02-11 23:10:35 +0100

    Updates

---
+++ precache-manifest.0c626c87b706538a3e88540457aeb4a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ecb9522c5e3f075eebb9f2244cca18",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "6103016229bda760aab9",
+    "url": "/markdown-previewer/static/js/2.dff3d11e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.dff3d11e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c3c5700382fab0c32d4",
+    "url": "/markdown-previewer/static/js/main.964ec4c5.chunk.js"
+  },
+  {
+    "revision": "c6b20c7a13fe9b316c34",
+    "url": "/markdown-previewer/static/js/runtime-main.ed575b9b.js"
+  }
+]);
